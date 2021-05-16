76b408df4fb2281aac55474d3d86c2fb12f1dee4
Gonzalo Arreche <gonzaloarreche@gmail.com>
2021-05-15 21:33:42 -0300

    Actualizando unas cosas

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,10 +1,10 @@
 create_dir ".tmux"
 
-git_clone "https://github.com/tmux-plugins/tpm" ".tmux/plugins/tpm"
-# Additionally, install plugins after clone:
-#   ~/.tmux/plugins/tpm/bin/install_plugins
-
 inject_line ".tmux.conf" "source-file $CURRENT_MODULE_PATH/tmux.colors.conf"
 inject_line ".tmux.conf" "source-file $CURRENT_MODULE_PATH/tmux.conf"
+
+# Additionally, install plugins after clone:
+#   ~/.tmux/plugins/tpm/bin/install_plugins
+inject_line ".tmux.conf" "run $CURRENT_MODULE_PATH/tpm/tpm"
 
 bundle_bash
